8a855fd97da4ef0f873a654487332c55fdffe0d9
Kevin Novak <kevinmnovak@yahoo.com>
2017-06-16 21:23:44 -0400

    Working on TreasureChest class

--- me/kevinnovak/treasurehunt/TreasureChest.java
+++ me/kevinnovak/treasurehunt/TreasureChest.java
@@ -1,9 +1,23 @@
 package me.kevinnovak.treasurehunt;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.inventory.Inventory;
+
 public class TreasureChest {
-	public TreasureChest plugin;
+	private TreasureChest plugin;
+	private Location location;
+	private Inventory contents;
+	private double startTime;
 	
-	TreasureChest(TreasureChest plugin) {
+	TreasureChest(TreasureChest plugin, Location location, Inventory contents, double startTime) {
         this.plugin = plugin;
+        this.location = location;
+        this.contents = contents;
+        this.startTime = startTime;
     }
+	
+	void spawn() {
+		this.location.getBlock().setType(Material.CHEST);
+	}
 }
