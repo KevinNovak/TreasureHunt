fa7b7844e4ccf7eeeb1986f32348e3efc63a0e01
Kevin Novak <kevinmnovak@yahoo.com>
2017-07-01 14:23:55 -0400

    Working on adding items

--- me/kevinnovak/treasurehunt/LootGenerator.java
+++ me/kevinnovak/treasurehunt/LootGenerator.java
@@ -4,6 +4,10 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.inventory.ItemStack;
+
 public class LootGenerator {
 	List<TreasureChestType> treasureChestTypes = new ArrayList<TreasureChestType>();
 	
@@ -20,6 +24,21 @@ public class LootGenerator {
 	}
 	
 	void setTreasureChestTypes(File[] files) {
-		
+		for (File file : files) {
+			if (file.exists()) {
+				FileConfiguration data = YamlConfiguration.loadConfiguration(file);
+				if (data.isSet("name") && data.isSet("weight") && data.isSet("value") && data.isSet("items")) {
+					String name = data.getString("name");
+					int weight = data.getInt("weight");
+					int value = data.getInt("value");
+					
+					List<ItemStack> items = new ArrayList<ItemStack>();
+					// Add items
+					
+					TreasureChestType treasureChestType = new TreasureChestType(name, weight, value, items);
+					treasureChestTypes.add(treasureChestType);
+				}
+			}
+		}
 	}
 }
