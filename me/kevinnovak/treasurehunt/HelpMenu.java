bef1fb07f1db77e014de87268ac37ae25fb68281
Kevin Novak <kevinmnovak@yahoo.com>
2017-07-03 14:30:49 -0400

    Working on help menu

--- me/kevinnovak/treasurehunt/HelpMenu.java
+++ me/kevinnovak/treasurehunt/HelpMenu.java
@@ -1,9 +1,14 @@
 package me.kevinnovak.treasurehunt;
 
+import org.bukkit.entity.Player;
+
 public class HelpMenu {
 
 	public HelpMenu() {
-
+		
 	}
 	
+	public void print(Player player) {
+		return;
+	}
 }
