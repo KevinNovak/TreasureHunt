af8e5a0908d630fd8bb94a617ac4b5c2f77b03db
Kevin Novak <kevinmnovak@yahoo.com>
2017-07-03 13:22:03 -0400

    Created permission manager class

---
+++ me/kevinnovak/treasurehunt/PermissionManager.java
@@ -0,0 +1,12 @@
+package me.kevinnovak.treasurehunt;
+
+public class PermissionManager {
+	public String base = "treasurehunt.";
+	
+	public String item = base + "item";
+	public String start = base + "start";
+	
+	public PermissionManager() {
+		
+	}
+}
