18ad399af1b71dd8c40048f9570c4598aea8cd24
Kevin Novak <kevinmnovak@yahoo.com>
2017-07-06 15:31:19 -0400

    Some permission reordering

--- me/kevinnovak/treasurehunt/PermissionManager.java
+++ me/kevinnovak/treasurehunt/PermissionManager.java
@@ -5,9 +5,9 @@ public class PermissionManager {
 	public String command = "command.";
 	
 	public String item = base + "item";
-	public String start = base + command + "start";
-	public String top = base + command + "top";
 	public String chests = base + command + "chests";
+	public String top = base + command + "top";
+	public String start = base + command + "start";
 	public String despawn = base + command + "despawn";
 	
 	public PermissionManager() {
